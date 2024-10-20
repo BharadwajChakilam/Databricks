b6f419d98e8a295f2922301841f6e385443ba390
BharadwajChakilam <BharadwajChakilam>
2024-10-20 01:57:27 +0000

    includes files

---
+++ Formula1/includes/configuration.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+raw_folder_path = "abfss://raw@formula1dl1102.dfs.core.windows.net"
+processed_folder_path="abfss://processed@formula1dl1102.dfs.core.windows.net"
+presentation_folder_path="abfss://presentation@formula1dl1102.dfs.core.windows.net"
+
+# COMMAND ----------
+
+demo_folder_path="abfss://demo@formula1dl1102.dfs.core.windows.net"
+
+# COMMAND ----------
+
+demo_folder_path
+
+# COMMAND ----------
+
+
